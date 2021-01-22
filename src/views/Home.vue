efcd43103d817bbb1c5cc2e9978893964487d303
Max <maxzadarnovskiyy@gmail.com>
2021-01-22 14:23:17 +1030

    Created a date getter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="home">
-    This is a Home Page
+    {{ currentDate }}
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
+// let API_KEY = "odzkbwziv9Da8Wtsc4CSzqOvuFnvVgGXobDvNw5o";
+const curday = function(sp) {
+  const today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //As January is 0.
+  const yyyy = today.getFullYear();
+
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  return yyyy + sp + mm + sp + dd;
+};
 
 export default Vue.extend({
   name: "Home",
+  computed: {
+    currentDate: () => {
+      return curday("-");
+    }
+  },
   metaInfo: {
     title: "DNEO - Home"
   }
